7d682bb38322e1929e108799dc191b6756d845f6
GonzaloCaroK <gonzalo.caro@kabeli.cl>
2025-06-22 21:42:35 -0400

    refactor: update database connection settings to use environment variables

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,11 +10,11 @@ import { ProductType } from './products/entities/product-type';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Holitas.01',
-  database: 'piscinasElMaipo',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   entities: [
     Product,
