d4187999e903cb646a3cc8fad7f164c2a8fcdbe4
GonzaloCaroK <gonzalo.caro@kabeli.cl>
2025-11-12 23:34:12 -0300

    fix: correct import paths for RevestimientoImagen and Revestimiento entities

--- src/cloudinary/clodinary.module.ts
+++ src/cloudinary/clodinary.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
 import { CloudinaryController } from './cloudinary.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RevestimientoImagen } from '@revestimientos/entities/revestimiento-imagen.entity';
-import { Revestimiento } from '@revestimientos/entities/revestimiento.entity';
+import { RevestimientoImagen } from '../revestimientos/entities/revestimiento-imagen.entity';
+import { Revestimiento } from '../revestimientos/entities/revestimiento.entity';
 import { CloudinaryProvider } from './cloudinary.provider';
 
 import * as dotenv from 'dotenv';
