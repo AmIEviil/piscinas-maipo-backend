5b15055634457387f4d58a43f56dda1a8b300233
GonzaloCaroK <gonzalo.caro@kabeli.cl>
2025-11-29 03:35:33 -0300

    fix: correct import path for JwtAuthGuard in users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,7 +10,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
-import { JwtAuthGuard } from 'auth/guards/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard)
